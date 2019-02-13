9064867878aa22bce847322a9377d27159db417a
Ben Buchanan <bsbuchanan99@gmail.com>
2019-02-13 11:27:30 -0500

    Planned functionality in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,36 @@
 #include <iostream>
 
+/* TODO: Create a hash algorithm to be used for computeHash() */
+int computeHash(char * name) {
+	int hash;
+	return hash;
+}
+
+/* TODO: Create a method of comparing the hashes to make a match */
+int computeMatch(int hash_one, int hash_two) {
+	int match;
+	return match;
+}
+
 int main(int argc, char * argv[]) {
 	char name_one[80];
 	char name_two[80];
+	int hash_one, hash_two, match;
 
 	std::cout << "Welcome to HashCupid! Please enter the names of the people you wish to match." << std::endl;
 	std::cout << "Name One: ";
 	std::cin >> name_one;
 	std::cout << "Name Two: ";
 	std::cin >> name_two;
+
+	hash_one = computeHash(name_one);
+	hash_two = computeHash(name_two);
+
+	match = computeMatch(hash_one, hash_two);
+
+	std::cout << "Match for " << name_one << " and " << name_two << ": " << match << "%" << std::endl;
+
+	/* TODO: Check what the percentage match was and print an appropriate message */
 
 	return 0;
 }
