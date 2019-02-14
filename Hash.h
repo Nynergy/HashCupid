0df9768a87ba2265a796798ffa11b41e4709012f
Ben Buchanan <bsbuchanan99@gmail.com>
2019-02-14 14:01:20 -0500

    Separated hash functions into a separate library.

---
+++ Hash.h
@@ -0,0 +1,16 @@
+#ifndef HASH_BB
+#define HASH_BB
+
+/* TODO: Create a hash algorithm to be used for computeHash() */
+int computeHash(char * name) {
+	int hash;
+	return hash;
+}
+
+/* TODO: Create a method of comparing the hashes to make a match */
+int computeMatch(int hash_one, int hash_two) {
+	int match;
+	return match;
+}
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,16 +1,5 @@
+#include "Hash.h"
 #include <iostream>
-
-/* TODO: Create a hash algorithm to be used for computeHash() */
-int computeHash(char * name) {
-	int hash;
-	return hash;
-}
-
-/* TODO: Create a method of comparing the hashes to make a match */
-int computeMatch(int hash_one, int hash_two) {
-	int match;
-	return match;
-}
 
 int main(int argc, char * argv[]) {
 	char name_one[80];
